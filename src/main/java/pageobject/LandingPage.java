b92048383acf0d5e3d95a0386faebbebabf3ca8f
Alina Deeva <d.andreevna.a@gmail.com>
2022-12-20 22:53:35 +0300

    add tests 2 to 5

--- src/main/java/pageobject/LandingPage.java
+++ src/main/java/pageobject/LandingPage.java
@@ -10,6 +10,32 @@ public class LandingPage extends BasePage {
     @FindBy(xpath = "//div[@id='header']//img")
     private WebElement bigLogo;
 
+    public WebElement getBigLogo() {
+        return bigLogo;
+    }
+
+    public void setBigLogo(WebElement bigLogo) {
+        this.bigLogo = bigLogo;
+    }
+
+    public WebElement getSunIcon() {
+        return sunIcon;
+    }
+
+    public void setSunIcon(WebElement sunIcon) {
+        this.sunIcon = sunIcon;
+    }
+
+    @FindBy(xpath = "//div[@class='sc-ksZaOG bxLYUZ']")
+    private WebElement sunIcon;
+
+    public WebElement getAboutUs() {
+        return aboutUs;
+    }
+
+    @FindBy(xpath = "//a[@class='sc-evZas hJsxZw'][1]")
+    private WebElement aboutUs;
+
 // Методы страницы
 
 
--- src/test/java/LandingPageTest.java
+++ src/test/java/LandingPageTest.java
@@ -1,20 +1,60 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-//import org.example.factory.WebDriverFactory;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import pageobject.BasePage;
 import pageobject.LandingPage;
 
-import java.time.Duration;
+import static driver.driver.*;
 
-public class LandingPageTest extends BasePageTest {
+public class LandingPageTest { //убрала наследование - нам же нужны нормальные названия тестов, а не бесконечное переопределение одного и того же
+    public BasePage basePage;
+    public LandingPage landingPage;
+
+    @BeforeTest
+    public void openLandingPage() {
+        createDriver();
+        basePage = new BasePage();
+        basePage.open();
+        landingPage = new LandingPage();
+    }
+
+    @AfterTest
+    public void quitPage() {
+        quite();
+    }
+
+    @Test
+    public void landingPageOpensWithDarkTheme() {
+        Assert.assertTrue(landingPage.getSunIcon().getAttribute("class").contains("bxLYUZ"));
+        System.out.println("________________________\n" +
+                "TEST PASSED: landing page has a default dark theme");
+    }
+
+    @Test
+    public void exlabBigLogoIsVisible() {
+        Assert.assertTrue(landingPage.getBigLogo().isDisplayed());
+        System.out.println("________________________\n" +
+                "TEST PASSED: ExLab big logo is visible");
+    }
+
+    @Test
+    public void menuItemAboutUsIsVisible() {
+        Assert.assertTrue(landingPage.getAboutUs().isDisplayed());
+        System.out.println("________________________\n" +
+                "TEST PASSED: Menu item 'О нас' is visible");
+    }
+
+    @Test
+    public void menuItemAboutUsNasValidLink() {
+        Assert.assertTrue(landingPage.getAboutUs().getAttribute("href").contains("#about"));
+        System.out.println("________________________\n" +
+                "TEST PASSED: Menu item 'О нас' has valid link");
+    }
+
+
+
 
 
     //Тесты для этой страницы. Они обращаются к локаторам и методам класса LandingPage
